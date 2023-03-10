62050f5a2e43c295cbdaac60ecdb0b13f0c3bb3a
londaploetz <lploetz@hotmail.com>
2023-03-10 12:40:00 -0600

    created dynamic gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/pages/Home"; 
-import About from "./components/pages/About"; 
-import Contact from "./components/pages/Contact"; 
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 import Gallery from "./components/pages/Gallery";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/navbar/Navbar"
+import  Footer  from "./components/footer/Footer";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes> 
-       <Route exact path="/About" element={<About />} />
-       <Route exact path="/Contact" element={<Contact />} />
-       <Route exact path="/Gallery" element={<Gallery />} />
-       <Route  exact path="/Home" element={<Home />} />
-       <Route  exact path="/" element={<Home />} />
+  return ( 
 
- </Routes>
-    </BrowserRouter>
+  
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/About" element={<About />} />
+        <Route exact path="/Contact" element={<Contact />} />
+        <Route exact path="/Gallery" element={<Gallery />} />
+        <Route exact path="/Home" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+
+      </Routes>
+
+    </BrowserRouter>  
+    
 
   );
 }
--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,15 +1,37 @@
-import React from "react"; 
-
-
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { pool1, pool2, pool3, pool4, pool5, pool6, pool7 } from "./imports.js"
+import Navbar from "../navbar/Navbar"
+import pools from "../../pool.json"
 
 function Gallery() {
   return (
-    <div className="gallery" >
-
-    pics
-    
+    <div>
+      <Container className="gallery-container">
+        <Row>
+          {pools.data.map((pool) => (
+            <GalleryCol
+              poolImgNum={pool.img}
+              altTag={pool.alt}
+            />
+          ))}
+        </Row>
+      </Container>
     </div>
   );
+}
+
+
+const GalleryCol = (props) => {
+
+  return (
+    
+      <Col lg={4}><img className="gallery-img" src={require(`../../assets/galleryimgs/pool${props.poolImgNum}.jpg`)} alt={props.altTag} /></Col>
+      
+
+  )
 }
 
 export default Gallery;
